fcad81ddaa181ade057b817aa85e19449f99b289
Marina Miftahova <miftahovamv00@gmail.com>
2020-06-23 18:31:19 +0700

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,57 @@
-function getResult(a,b,c){
+function getResult(a, b, c) {
     // код для задачи №1 писать здесь
-    // return x;
+    "use strict"
+    let coeff1 = a;
+    let coeff2 = b;
+    let coeff3 = c;
+    let discr = coeff2 ** 2 - 4 * coeff1 * coeff3;
+    let result;
+    let x1;
+    let x2;
+    if (discr < 0) {
+        result = [];
+        return result
+    } else if (discr === 0) {
+        x1 = (-coeff2 + Math.sqrt(discr)) / (2 * coeff1);
+        result = [x1];
+        return result;
+    } else {
+        x1 = (-coeff2 + Math.sqrt(discr)) / (2 * coeff1);
+        x2 = (-coeff2 - Math.sqrt(discr)) / (2 * coeff1);
+        result = [x1, x2];
+        return result;
+    }
 }
+// return x;
 
-function getAverageMark(marks){
+function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark;
+    let averageMark;
+    let sum = 0;
+    if (marks.length === 0) {
+        return 0;
+    } else if (marks.length > 5) {
+        marks.splice(5);
+    }
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+        averageMark = sum / marks.length;
+    }
+    return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
+
+
+function askDrink(name, dateOfBirthday) {
     // код для задачи №3 писать здесь
-    // return result;
+    let nameUser = name;
+    let yearUser = dateOfBirthday.getFullYear();
+    let newDate = new Date().getFullYear();
+    //let result = [`Не желаете ли олд-фэшн, ${nameUser}?`, `Сожалею, ${nameUser}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!` ]
+    if ((newDate - yearUser) >= 18) {
+        return `Не желаете ли олд-фэшн, ${nameUser}?`;
+    } else if ((newDate - yearUser) < 18) {
+        return `Сожалею, ${nameUser}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
 }
+    // return result;
